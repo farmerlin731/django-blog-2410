7966845817340a665f1abc0764386afd7edc398f
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2024-11-30 14:18:12 +0800

    Use bootstrap to make a table @ post_list page.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, "blog/post_list.html")
+    # return render(request, "blog/post_list.html")
+    posts = Post.objects.all()
+    return render(request, "blog/post_list.html", {"posts": posts})
